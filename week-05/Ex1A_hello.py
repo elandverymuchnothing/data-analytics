a0eb40e417a1ad66608200713d3d6a7ffe57eca3
elandverymuchnothing <rollinsariel97@gmail.com>
2026-05-08 17:19:08 -0500

    5-8

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,2 +1,25 @@
-print('Hello world!')
+# print('Hello world!')
 
+# message = 'Hello world!'
+# print(message)
+
+# "Hello world prints twice because the commmand is written twice"
+
+# Displaying dollars and cents
+
+dollars = 3
+cents = .50
+
+print(dollars + cents)
+
+# I notice that the terminal doesn't register the number "0"
+
+cents = cents + .25
+print(dollars + cents)
+
+# I notice the output shows 3.75
+
+d_str = '3 dollars'
+c_str = '50 cents'
+
+print(d_str + " " + c_str)
