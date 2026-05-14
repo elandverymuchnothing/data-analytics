75d6324e4c49a0a800d575a617078395951a1919
elandverymuchnothing <rollinsariel97@gmail.com>
2026-05-14 14:43:10 -0500

    areaofrectangle

---
+++ week-05/Ex2A-MathScripts/area_of_rectangle.py
@@ -0,0 +1,11 @@
+#Rectangle sides based on birthday example
+side_a = 6   
+side_b = 3
+
+#Calculating area
+area = side_a * side_b
+
+#Output
+print("Side A is", side_a)
+print("Side B is", side_b)
+print("The area of the rectangle is", area)
