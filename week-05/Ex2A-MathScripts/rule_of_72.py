5be0f941ad73d1ba7c53fb0c1182307d0f9fb9cc
elandverymuchnothing <rollinsariel97@gmail.com>
2026-05-15 14:10:43 -0500

    rule of 72 f string

--- week-05/Ex2A-MathScripts/rule_of_72.py
+++ week-05/Ex2A-MathScripts/rule_of_72.py
@@ -17,8 +17,31 @@
 
 
 #Values
+#savings = float(input("How much money is in your savings account? "))
+#rate = float(input("What is the interest rate (as a percent)? "))
+
+#Percent to decimal conversion
+#rate = rate / 100
+
+#Rule of 72
+#years_to_double = 72 / (rate * 100)
+
+#Doubled savings amount
+#future_value = savings * 2
+
+#Output
+#print("Your current savings is", format(savings, ".2f") + ".")
+#print("At a", format(rate, ".0%"), "interest rate, your savings account will be")
+#print("worth", format(future_value, ".2f"), "in", format(years_to_double, ".1f"), "years")
+
+# Possible pitfalls with input():
+# 1. input() always starts as text; math will not work unless converted using float() or int()
+# 2. Entering words instead of numbers will cause an error
+# 3. Probably "0 by division" error if interest rate is 0%
+
+#Values
 savings = float(input("How much money is in your savings account? "))
-rate = float(input("What is the interest rate (as a percent)? "))
+rate = float(input("What is the interest rate (%)? "))
 
 #Percent to decimal conversion
 rate = rate / 100
@@ -29,11 +52,7 @@ years_to_double = 72 / (rate * 100)
 #Doubled savings amount
 future_value = savings * 2
 
-print("Your current savings is", format(savings, ".2f") + ".")
-print("At a", format(rate, ".0%"), "interest rate, your savings account will be")
-print("worth", format(future_value, ".2f"), "in", format(years_to_double, ".1f"), "years")
-
-# Possible pitfalls with input():
-# 1. input() always starts as text; math will not work unless converted using float() or int()
-# 2. Entering words instead of numbers will cause an error
-# 3. Probably "0 by division" error if interest rate is 0%
+#F-string output25
+print(f"Your current savings is {format(savings, '.2f')}.")
+print(f"At a {format(rate, '.0%')} interest rate, your savings account will be")
+print(f"worth {format(future_value, '.2f')} in {format(years_to_double, '.1f')} years")
